532b22fbf80b8c75624529e077052ad53d53f395
jeanm123 <jeancv98@gmail.com>
2021-09-06 18:40:41 -0500

    reemplazando

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,41 @@
 
-function subir_arriba(){
-    var boton_arriba= document.documentElement.scrollTop ||
-	document.body.scrollTop;
-    if(boton_arriba>0){
-        // window.requestAnimationFrame(subir_arriba);
-        // window.scrollTo(0,boton_arriba-(boton_arriba/5));
-        window.scrollTo(0,0);
-    }
-}
-buttonUp=document.getElementById("boton_arriba");
-window.onscroll=function(){
-    var scroll= document.documentElement.scrollTop ||
-	document.body.scrollTop;document.documentElement.scrollY;
+$(document).ready(function(){
+	$('#boton_arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
 
-    if(scroll>100){
-        buttonUp.style.transform="scale(1)";
-    }else{
-        buttonUp.style.transform="scale(0)";
-    }
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('#boton_arriba').slideDown(300);
+		} else {
+			$('#boton_arriba').slideUp(300);
+		}
+	});
+
+});
+// function subir_arriba(){
+//     var boton_arriba= document.documentElement.scrollTop ||
+// 	document.body.scrollTop;
+//     if(boton_arriba>0){
+//         // window.requestAnimationFrame(subir_arriba);
+//         // window.scrollTo(0,boton_arriba-(boton_arriba/5));
+//         window.scrollTo(0,0);
+//     }
+// }
+// buttonUp=document.getElementById("boton_arriba");
+// window.onscroll=function(){
+//     var scroll= document.documentElement.scrollTop ||
+// 	document.body.scrollTop;document.documentElement.scrollY;
+
+//     if(scroll>100){
+//         buttonUp.style.transform="scale(1)";
+//     }else{
+//         buttonUp.style.transform="scale(0)";
+//     }
     
-}
+// }
 
 // EVENTO PARA MOSTRAR 
 function open_close_menu(){
